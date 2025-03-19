dfcfbbb47c9b81c7972e85a97737a8889d4c164a
Hailey Pan <haileyrpan@gmail.com>
2025-03-19 13:25:44 -0400

    nice legend

---
+++ src/lib/Pie.svelte
@@ -0,0 +1,76 @@
+<script>
+  import * as d3 from "d3";
+
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+  let data = [
+    { value: 1, label: "apples" },
+    { value: 2, label: "oranges" },
+    { value: 3, label: "mangos" },
+    { value: 4, label: "pears" },
+    { value: 5, label: "limes" },
+    { value: 5, label: "cherries" },
+  ];
+  let sliceGenerator = d3.pie().value((d) => d.value);
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+</script>
+
+<svg viewBox="-50 -50 100 100">
+  {#each arcs as arc, index}
+    <path d={arc} fill={colors(index)} />
+  {/each}
+</svg>
+
+<ul class="legend">
+	{#each data as d, index}
+		<li style="--color: { colors(index) }">
+			<span class="swatch"></span>
+			{d.label} <em>({d.value})</em>
+		</li>
+	{/each}
+</ul>
+
+
+<style>
+  svg {
+    max-width: 20em;
+    margin-block: 2em;
+
+    /* Do not clip shapes outside the viewBox */
+    overflow: visible;
+  }
+
+  /* Legend styling */
+  .legend {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(8em, 1fr));
+    gap: 1em;
+    padding: 1em;
+    border: 1px solid black;
+    margin-top: 2em;
+  }
+
+  .legend li {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+  }
+
+  .swatch {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    background-color: var(--color);
+    border-radius: 50%;
+  }
+
+  .legend em {
+    margin-left: 0.25em;
+  }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,17 +1,20 @@
 <script>
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
+  import Pie from '$lib/Pie.svelte';
 </script>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
+
 <h1>{projects.length} Projects</h1>
+
+<Pie />
 <div class="projects">
   {#each projects as p}
     <Project data={p} />
   {/each}
-
   <!-- <article>
     <h2>Amet totam debitis temporibus.</h2>
     <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="" />
